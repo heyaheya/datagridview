c342e270583d23e7a4aa050d7db4f6515e4e48b1
Radosław Wrocławski <heyaheya@wp.pl>
2018-09-11 09:54:19 +0200

    Dodaj pliki projektów.

---
+++ WindowsApplication6/Form1.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+		Me.components = New System.ComponentModel.Container()
+		Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+		Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+		Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+		Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+		Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+		Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+		Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+		Me.Chart2 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+		Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+		Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
+		Me.LabelTimer = New System.Windows.Forms.Label()
+		Me.ListView2 = New System.Windows.Forms.ListView()
+		Me.Button3 = New System.Windows.Forms.Button()
+		Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+		Me.ListView1 = New System.Windows.Forms.ListView()
+		Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+		Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+		Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+		Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+		CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.Chart2, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SplitContainer1.Panel1.SuspendLayout()
+		Me.SplitContainer1.Panel2.SuspendLayout()
+		Me.SplitContainer1.SuspendLayout()
+		CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SplitContainer2.Panel1.SuspendLayout()
+		Me.SplitContainer2.Panel2.SuspendLayout()
+		Me.SplitContainer2.SuspendLayout()
+		CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.TableLayoutPanel1.SuspendLayout()
+		Me.SuspendLayout()
+		'
+		'Chart1
+		'
+		ChartArea1.Name = "ChartArea1"
+		Me.Chart1.ChartAreas.Add(ChartArea1)
+		Legend1.Alignment = System.Drawing.StringAlignment.Center
+		Legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom
+		Legend1.MaximumAutoSize = 10.0!
+		Legend1.Name = "Legend1"
+		Legend1.TitleSeparator = System.Windows.Forms.DataVisualization.Charting.LegendSeparatorStyle.Line
+		Me.Chart1.Legends.Add(Legend1)
+		Me.Chart1.Location = New System.Drawing.Point(597, 45)
+		Me.Chart1.Name = "Chart1"
+		Series1.ChartArea = "ChartArea1"
+		Series1.Legend = "Legend1"
+		Series1.Name = "Series1"
+		Me.Chart1.Series.Add(Series1)
+		Me.Chart1.Size = New System.Drawing.Size(419, 90)
+		Me.Chart1.TabIndex = 11
+		Me.Chart1.Text = "Chart1"
+		'
+		'Chart2
+		'
+		ChartArea2.Name = "ChartArea1"
+		Me.Chart2.ChartAreas.Add(ChartArea2)
+		Legend2.Name = "Legend1"
+		Me.Chart2.Legends.Add(Legend2)
+		Me.Chart2.Location = New System.Drawing.Point(116, 108)
+		Me.Chart2.Name = "Chart2"
+		Series2.ChartArea = "ChartArea1"
+		Series2.Legend = "Legend1"
+		Series2.Name = "Series1"
+		Me.Chart2.Series.Add(Series2)
+		Me.Chart2.Size = New System.Drawing.Size(643, 100)
+		Me.Chart2.TabIndex = 12
+		Me.Chart2.Text = "Chart2"
+		'
+		'SplitContainer1
+		'
+		Me.SplitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.SplitContainer1.Location = New System.Drawing.Point(12, 12)
+		Me.SplitContainer1.Name = "SplitContainer1"
+		Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+		'
+		'SplitContainer1.Panel1
+		'
+		Me.SplitContainer1.Panel1.Controls.Add(Me.SplitContainer2)
+		'
+		'SplitContainer1.Panel2
+		'
+		Me.SplitContainer1.Panel2.Controls.Add(Me.TableLayoutPanel1)
+		Me.SplitContainer1.Size = New System.Drawing.Size(1149, 603)
+		Me.SplitContainer1.SplitterDistance = 278
+		Me.SplitContainer1.TabIndex = 13
+		'
+		'SplitContainer2
+		'
+		Me.SplitContainer2.Location = New System.Drawing.Point(3, 3)
+		Me.SplitContainer2.Name = "SplitContainer2"
+		Me.SplitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal
+		'
+		'SplitContainer2.Panel1
+		'
+		Me.SplitContainer2.Panel1.Controls.Add(Me.LinkLabel1)
+		Me.SplitContainer2.Panel1.Controls.Add(Me.LabelTimer)
+		Me.SplitContainer2.Panel1.Controls.Add(Me.ListView2)
+		Me.SplitContainer2.Panel1.Controls.Add(Me.Chart1)
+		Me.SplitContainer2.Panel1.Controls.Add(Me.Button3)
+		Me.SplitContainer2.Panel1.Controls.Add(Me.ProgressBar1)
+		Me.SplitContainer2.Panel1.Controls.Add(Me.Button2)
+		Me.SplitContainer2.Panel1.Controls.Add(Me.Button1)
+		Me.SplitContainer2.Panel1.Controls.Add(Me.Label1)
+		'
+		'SplitContainer2.Panel2
+		'
+		Me.SplitContainer2.Panel2.Controls.Add(Me.DataGridView1)
+		Me.SplitContainer2.Panel2.Enabled = False
+		Me.SplitContainer2.Panel2MinSize = 0
+		Me.SplitContainer2.Size = New System.Drawing.Size(1105, 250)
+		Me.SplitContainer2.SplitterDistance = 150
+		Me.SplitContainer2.TabIndex = 0
+		'
+		'LabelTimer
+		'
+		Me.LabelTimer.AutoSize = True
+		Me.LabelTimer.Location = New System.Drawing.Point(4, 111)
+		Me.LabelTimer.Name = "LabelTimer"
+		Me.LabelTimer.Size = New System.Drawing.Size(39, 13)
+		Me.LabelTimer.TabIndex = 19
+		Me.LabelTimer.Text = "Label2"
+		'
+		'ListView2
+		'
+		Me.ListView2.Location = New System.Drawing.Point(151, 16)
+		Me.ListView2.Name = "ListView2"
+		Me.ListView2.Size = New System.Drawing.Size(395, 119)
+		Me.ListView2.TabIndex = 18
+		Me.ListView2.UseCompatibleStateImageBehavior = False
+		'
+		'Button3
+		'
+		Me.Button3.Location = New System.Drawing.Point(3, 84)
+		Me.Button3.Name = "Button3"
+		Me.Button3.Size = New System.Drawing.Size(75, 23)
+		Me.Button3.TabIndex = 17
+		Me.Button3.Text = "Button3"
+		Me.Button3.UseVisualStyleBackColor = True
+		'
+		'ProgressBar1
+		'
+		Me.ProgressBar1.Location = New System.Drawing.Point(680, 16)
+		Me.ProgressBar1.Name = "ProgressBar1"
+		Me.ProgressBar1.Size = New System.Drawing.Size(276, 23)
+		Me.ProgressBar1.TabIndex = 14
+		'
+		'Button2
+		'
+		Me.Button2.Location = New System.Drawing.Point(3, 55)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(75, 23)
+		Me.Button2.TabIndex = 15
+		Me.Button2.Text = "Button2"
+		Me.Button2.UseVisualStyleBackColor = True
+		'
+		'Button1
+		'
+		Me.Button1.Location = New System.Drawing.Point(3, 16)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(75, 23)
+		Me.Button1.TabIndex = 14
+		Me.Button1.Text = "Button1"
+		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'CheckBox1
+		'
+		Me.CheckBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+		Me.CheckBox1.AutoSize = True
+		Me.CheckBox1.Location = New System.Drawing.Point(3, 85)
+		Me.CheckBox1.Name = "CheckBox1"
+		Me.CheckBox1.Size = New System.Drawing.Size(95, 17)
+		Me.CheckBox1.TabIndex = 13
+		Me.CheckBox1.Text = "Wyswietl dane"
+		Me.CheckBox1.UseVisualStyleBackColor = True
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Dock = System.Windows.Forms.DockStyle.Top
+		Me.Label1.Location = New System.Drawing.Point(0, 0)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(39, 13)
+		Me.Label1.TabIndex = 12
+		Me.Label1.Text = "Label1"
+		'
+		'DataGridView1
+		'
+		Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+		Me.DataGridView1.Location = New System.Drawing.Point(21, 12)
+		Me.DataGridView1.Name = "DataGridView1"
+		Me.DataGridView1.Size = New System.Drawing.Size(1071, 77)
+		Me.DataGridView1.TabIndex = 0
+		'
+		'ListView1
+		'
+		Me.ListView1.Location = New System.Drawing.Point(116, 3)
+		Me.ListView1.Name = "ListView1"
+		Me.ListView1.Size = New System.Drawing.Size(535, 99)
+		Me.ListView1.TabIndex = 13
+		Me.ListView1.UseCompatibleStateImageBehavior = False
+		'
+		'Timer1
+		'
+		'
+		'LinkLabel1
+		'
+		Me.LinkLabel1.AutoSize = True
+		Me.LinkLabel1.Location = New System.Drawing.Point(38, 111)
+		Me.LinkLabel1.Name = "LinkLabel1"
+		Me.LinkLabel1.Size = New System.Drawing.Size(59, 13)
+		Me.LinkLabel1.TabIndex = 20
+		Me.LinkLabel1.TabStop = True
+		Me.LinkLabel1.Text = "LinkLabel1"
+		'
+		'TableLayoutPanel1
+		'
+		Me.TableLayoutPanel1.ColumnCount = 2
+		Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13.33333!))
+		Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 86.66666!))
+		Me.TableLayoutPanel1.Controls.Add(Me.CheckBox1, 0, 0)
+		Me.TableLayoutPanel1.Controls.Add(Me.ListView1, 1, 0)
+		Me.TableLayoutPanel1.Controls.Add(Me.Chart2, 1, 1)
+		Me.TableLayoutPanel1.Location = New System.Drawing.Point(24, 19)
+		Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+		Me.TableLayoutPanel1.RowCount = 2
+		Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+		Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+		Me.TableLayoutPanel1.Size = New System.Drawing.Size(855, 211)
+		Me.TableLayoutPanel1.TabIndex = 14
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(1181, 618)
+		Me.Controls.Add(Me.SplitContainer1)
+		Me.Name = "Form1"
+		Me.Text = "Form1"
+		CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.Chart2, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.SplitContainer1.Panel1.ResumeLayout(False)
+		Me.SplitContainer1.Panel2.ResumeLayout(False)
+		CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.SplitContainer1.ResumeLayout(False)
+		Me.SplitContainer2.Panel1.ResumeLayout(False)
+		Me.SplitContainer2.Panel1.PerformLayout()
+		Me.SplitContainer2.Panel2.ResumeLayout(False)
+		CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.SplitContainer2.ResumeLayout(False)
+		CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.TableLayoutPanel1.ResumeLayout(False)
+		Me.TableLayoutPanel1.PerformLayout()
+		Me.ResumeLayout(False)
+
+	End Sub
+
+	Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+	Friend WithEvents Chart2 As DataVisualization.Charting.Chart
+	Friend WithEvents SplitContainer1 As SplitContainer
+	Friend WithEvents Label1 As Label
+	Friend WithEvents SplitContainer2 As SplitContainer
+	Friend WithEvents CheckBox1 As CheckBox
+	Friend WithEvents DataGridView1 As DataGridView
+	Friend WithEvents ListView1 As ListView
+	Friend WithEvents Button1 As Button
+	Friend WithEvents Button2 As Button
+	Friend WithEvents ProgressBar1 As ProgressBar
+	Friend WithEvents Button3 As Button
+	Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+	Friend WithEvents ListView2 As ListView
+	Friend WithEvents LabelTimer As Label
+	Friend WithEvents Timer1 As Timer
+	Friend WithEvents LinkLabel1 As LinkLabel
+	Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+End Class
---
+++ WindowsApplication6/Form1.vb
@@ -0,0 +1,452 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Data
+Imports System.Windows.Forms.DataVisualization.Charting
+Imports Oracle.DataAccess.Client ' ODP.NET Oracle managed provider
+Imports Oracle.DataAccess.Types
+
+
+
+Public Class Form1
+
+    Public ds As DataSet
+    Public dr As OracleDataReader
+	Public Property AxisX As Object
+
+	'zmienna do Timer1
+	Dim second As Integer
+
+	Dim x0 As DateTime
+	Dim mouseIsDown As Boolean
+	Dim p0 As Point
+	Dim p As Point
+
+
+
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+		Timer1.Interval = 1000
+		Timer1.Start() 'Timer starts functioning
+
+		Call Proc_22()
+
+
+
+		'SplitContainer1.Dock = DockStyle.Fill
+		'SplitContainer2.Dock = DockStyle.Fill
+
+		'Label1.Dock = DockStyle.Top
+		'DataGridView1.Dock = DockStyle.Fill
+
+
+
+
+
+
+
+		SplitContainer2.Panel2.Visible = False
+		SplitContainer2.SplitterDistance = SplitContainer2.Height
+		CheckBox1.Text = "Wyświetl dane"
+
+		'Chart1.ChartAreas[0].AxisX.LabelStyle.Format = "yyyy-MM-dd";
+		Me.Chart1.Series(0).XValueType = ChartValueType.DateTime
+
+		Me.Chart1.ChartAreas(0).AxisX.LabelStyle.Format = "yyyy-MM-dd"
+
+		'obrot etykiety o wartość stopni do -90 do +90
+		'Me.Chart1.ChartAreas(0).AxisX.LabelStyle.Angle = 0
+
+		Me.Chart1.ChartAreas(0).AxisX.Interval = 1
+		Me.Chart1.ChartAreas(0).AxisX.IntervalType = DateTimeIntervalType.Days
+		Me.Chart1.ChartAreas(0).AxisX.IntervalOffset = 1
+
+		'wyłączenie znaczników osi X
+		'Me.Chart2.ChartAreas(0).AxisX.MajorGrid.Enabled = False
+
+
+		Me.Chart1.ChartAreas(0).AxisX.LabelAutoFitStyle = True
+
+
+
+
+		Dim ex As Exception
+		Dim ls As New List(Of String)
+
+		Dim Serwer_FTP As String
+		Dim Login_FTP As String
+		Dim Haslo_FTP As String
+
+		Serwer_FTP = "ftp://energaobrot.home.pl"
+		Login_FTP = "oht_miernik_mocy@energaobrot.pl"
+		Haslo_FTP = "M13rN!k3n#rG@"
+
+		ls = ListRemoteFiles(Serwer_FTP, Login_FTP, Haslo_FTP, ex)
+
+
+
+		'MsgBox(ex)
+
+	End Sub
+
+	Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+		LabelTimer.Text = DateTime.Now.ToString
+
+		second = second + 1
+		If second >= 10 Then
+			Timer1.Stop() 'Timer stops functioning
+			MsgBox("Timer Stopped....")
+		End If
+
+	End Sub
+
+
+	Sub Proc_22()
+
+
+
+		Dim i_seria As Integer = 0
+
+		Dim o_chart As Chart
+
+		Dim data_od As Date
+		Dim data_do As Date
+		Dim l_ID_Prognoza As Long
+		Dim l_ID_Wykonanie As Long
+		Dim l_ID_CRE As Long
+		Dim s_Agregat As String
+
+
+
+		data_od = Now().AddDays(-10)
+		data_do = data_od.AddDays(+8)
+
+		l_ID_Prognoza = 3014
+		l_ID_CRE = 3006
+		l_ID_Wykonanie = 3010
+		s_Agregat = "hh"
+
+		o_chart = Me.Chart1
+
+		data_od = Now().AddDays(-10)
+		data_do = data_od.AddDays(+8)
+
+		ustaw_parametry_wykresu(o_chart, 500, 600)
+		dodaj_serie(o_chart, 0, "prognoza", Color.Blue, False)
+		pobierz_dane_do_serii(o_chart, 0, 3, 0, data_od, data_do, l_ID_CRE, l_ID_Prognoza, l_ID_Wykonanie, s_Agregat)
+
+		dodaj_serie(o_chart, 1, "wykonanie", Color.Green, False)
+		pobierz_dane_do_serii(o_chart, 1, 3, 1, data_od, data_do, l_ID_CRE, l_ID_Prognoza, l_ID_Wykonanie, s_Agregat)
+
+		dodaj_serie(o_chart, 2, "odchylenie", Color.Red, False)
+		pobierz_dane_do_serii(o_chart, 2, 3, 2, data_od, data_do, l_ID_CRE, l_ID_Prognoza, l_ID_Wykonanie, s_Agregat)
+
+
+		o_chart = Me.Chart2
+		ustaw_parametry_wykresu(o_chart, 500, 600)
+		dodaj_serie(o_chart, 0, "prognoza", Color.Blue, False)
+		pobierz_dane_do_serii(o_chart, 0, 3, 0, data_od, data_do, l_ID_CRE, l_ID_Prognoza, l_ID_Wykonanie, s_Agregat)
+
+		dodaj_serie(o_chart, 1, "wykonanie", Color.Green, False)
+		pobierz_dane_do_serii(o_chart, 1, 3, 1, data_od, data_do, l_ID_CRE, l_ID_Prognoza, l_ID_Wykonanie, s_Agregat)
+
+		dodaj_serie(o_chart, 2, "odchylenie", Color.Red, False)
+		pobierz_dane_do_serii(o_chart, 2, 3, 2, data_od, data_do, l_ID_CRE, l_ID_Prognoza, l_ID_Wykonanie, s_Agregat)
+
+
+		'dodanie lini poziomej na lini 333 o grubosci 8
+		'Chart1.ChartAreas(0).AxisY.StripLines.Add(New StripLine())
+		'Chart1.ChartAreas(0).AxisY.StripLines(0).BackColor = Color.FromArgb(80, 252, 180, 65)
+		'Chart1.ChartAreas(0).AxisY.StripLines(0).StripWidth = 8
+		'Chart1.ChartAreas(0).AxisY.StripLines(0).Interval = 1000
+		'Chart1.ChartAreas(0).AxisY.StripLines(0).IntervalOffset = 333
+
+		'dodanie lini pionowej na lini 97 o grubosci 1
+		Chart1.ChartAreas(0).AxisX.StripLines.Add(New StripLine())
+		Chart1.ChartAreas(0).AxisX.StripLines(0).BackColor = Color.FromArgb(80, 252, 180, 65)
+		Chart1.ChartAreas(0).AxisX.StripLines(0).StripWidth = 10
+		Chart1.ChartAreas(0).AxisX.StripLines(0).Interval = 100
+
+		Dim data2 As Date = "2018-08-27 00:00"
+		'Dim laba As Double
+
+
+		'
+		'laba = data2.ToOADate
+
+		'Chart1.ChartAreas(0).AxisX.StripLines(0).IntervalOffset = laba
+
+
+
+
+
+	End Sub
+
+	Sub ustaw_parametry_wykresu(o_chart As Chart, i_wysokosc As Integer, i_szeroksc As Integer)
+
+		'rozmiar i wlasciwosci
+		'legenda
+		'etykieta
+		'typ
+		'etykiety osi 
+
+
+
+		With o_chart.ChartAreas(0)
+			.AxisX.MajorGrid.LineColor = Color.LightBlue
+			.AxisY.MajorGrid.LineColor = Color.LightGray
+
+			'.AxisX.Minimum = 1
+			'.AxisX.Maximum = 24 * 7
+			'.AxisX.Interval = 1
+
+			'.AxisY.Minimum = 0
+			'.AxisY.Maximum = 100
+			'.AxisY.Interval = 20
+
+		End With
+
+		'o_chart.Width = i_szeroksc
+		' o_chart.Height = i_wysokosc
+
+		o_chart.Dock = DockStyle.Fill
+
+
+	End Sub
+
+	Sub dodaj_serie(o_chart As Chart, i_seria As Integer, s_nazwa As String, o_kolor As Color, os_pomocnicza As Boolean)
+
+
+		If o_chart.Legends.Count >= i_seria + 1 Then
+
+			'istniejąca seria
+			o_chart.Series(i_seria).Points.Clear()
+			o_chart.Series(i_seria).Name = s_nazwa
+		Else
+			'nowa seria
+			o_chart.Series.Add(s_nazwa)
+
+		End If
+
+		o_chart.Series(i_seria).Color = o_kolor
+
+		o_chart.Series(i_seria).BorderWidth = 3
+
+		If os_pomocnicza = True Then
+			o_chart.Series(i_seria).YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary
+		End If
+
+		o_chart.Series(i_seria).ChartType = SeriesChartType.Line
+
+	End Sub
+
+
+	Sub pobierz_dane_do_serii(o_chart As Chart, i_seria As Integer, i_kolumna_x As Integer, i_kolumna_y As Integer,
+							  data_od As Date, data_do As Date, l_ID_CRE As Long, l_ID_Prognoza As Long,
+							  l_ID_Wykonanie As Long, s_Agregat As String)
+
+
+		Dim da As OracleDataAdapter
+		'Dim cb As OracleCommandBuilder
+
+		Dim sql As String
+
+		'ERGH
+		Dim oradb As String = "Data Source=(DESCRIPTION=(ADDRESS_LIST=" _
+					+ "(ADDRESS=(PROTOCOL=TCP)(HOST = ENCELADUS.energa.loc)(PORT = 1521)))" _
+					+ "(CONNECT_DATA=(SERVICE_NAME=SRV_ERGH)));" _
+					+ "User Id=skome;Password=szafran1;"
+
+		Dim conn As New OracleConnection(oradb) ' Visual Basic
+
+		Try
+			conn.Open()
+
+		Catch ex As OracleException ' catches only Oracle errors
+			Select Case ex.Number
+				Case 1
+					MessageBox.Show("Error attempting to insert duplicate data.")
+				Case 12560
+					MessageBox.Show("Baza danych jest niedostępna !")
+				Case Else
+					MessageBox.Show("Database error: " + ex.Message.ToString())
+			End Select
+
+
+		Catch ex As Exception
+			MessageBox.Show(ex.Message.ToString())
+		Finally
+			'conn.Dispose()
+		End Try
+
+
+		Dim cmd As New OracleCommand
+		cmd.Connection = conn
+
+
+
+		sql = ""
+		sql = sql & "  SELECT SUM (Prognoza)           AS " & Chr(34) & "Prognoza" & Chr(34) & ","
+		sql = sql & "         SUM (Wykonanie)          AS " & Chr(34) & "Wykonanie" & Chr(34) & ","
+		sql = sql & "         SUM (Prognoza - Wykonanie) As " & Chr(34) & "Odchylenie" & Chr(34) & ","
+		sql = sql & "         TRUNC (K.GODZINA, '" & s_Agregat & "')  AS " & Chr(34) & "data" & Chr(34)
+		sql = sql & "    FROM (  Select SUM (VALUE) As CRE, TRUNC (DateTime, 'hh') AS GODZINA"
+		sql = sql & "              FROM energy15"
+		sql = sql & "             WHERE     FORMULAID = '" & l_ID_CRE & "'"
+		sql = sql & "                   AND DateTime >="
+		sql = sql & "                           TO_DATE ('" & data_od.ToString("yyyy-MM-dd") & " 00:00', 'YYYY-MM-DD HH24:MI')"
+		sql = sql & "                   AND DateTime <"
+		sql = sql & "                           TO_DATE ('" & data_do.ToString("yyyy-MM-dd") & " 00:00', 'YYYY-MM-DD HH24:MI')"
+		sql = sql & "          GROUP BY TRUNC (DATETIME, 'hh')) K"
+		sql = sql & "         INNER JOIN"
+		sql = sql & "         (  SELECT SUM (VALUE) / 1000 AS Prognoza,"
+		sql = sql & "                   TRUNC (DateTime, 'hh') AS GODZINA"
+		sql = sql & "              FROM energy15"
+		sql = sql & "             WHERE     FORMULAID = '" & l_ID_Prognoza & "'"
+		sql = sql & "                   AND DateTime >="
+		sql = sql & "                           TO_DATE ('" & data_od.ToString("yyyy-MM-dd") & " 00:00', 'YYYY-MM-DD HH24:MI')"
+		sql = sql & "                   AND DateTime <"
+		sql = sql & "                           TO_DATE ('" & data_do.ToString("yyyy-MM-dd") & " 00:00', 'YYYY-MM-DD HH24:MI')"
+		sql = sql & "          GROUP BY TRUNC (DATETIME, 'hh')) D"
+		sql = sql & "             ON (K.GODZINA = D.GODZINA)"
+		sql = sql & "         INNER JOIN"
+		sql = sql & "         (  SELECT SUM (VALUE)/1000 AS Wykonanie, TRUNC (DATETIME, 'hh') AS GODZINA"
+		sql = sql & "              FROM energy15"
+		sql = sql & "             WHERE     FORMULAID = '" & l_ID_Wykonanie & "'"
+		sql = sql & "                   AND datetime >="
+		sql = sql & "                           TO_DATE ('" & data_od.ToString("yyyy-MM-dd") & " 00:00', 'YYYY-MM-DD HH24:MI')"
+		sql = sql & "                   AND DateTime <"
+		sql = sql & "                           TO_DATE ('" & data_do.ToString("yyyy-MM-dd") & " 00:00', 'YYYY-MM-DD HH24:MI')"
+		sql = sql & "          GROUP BY TRUNC (DATETIME, 'hh')) RB"
+		sql = sql & "             ON K.GODZINA = RB.GODZINA "
+		sql = sql & "GROUP BY TRUNC (K.GODZINA, '" & s_Agregat & "') "
+		sql = sql & "ORDER BY TRUNC (K.GODZINA, '" & s_Agregat & "')"
+
+
+		cmd.CommandText = sql
+
+		cmd.CommandType = CommandType.Text
+
+		da = New OracleDataAdapter(cmd)
+		'cb = New OracleCommandBuilder(da)
+		ds = New DataSet()
+
+		da.Fill(ds)
+
+		dr = cmd.ExecuteReader()
+
+		Dim i As Int32
+		i = 0
+
+
+		Dim data1 As Date
+		Dim aaa As Integer
+
+		aaa = dr.FieldCount - 1
+
+		Dim matrix(aaa, 0) As String
+
+		While dr.Read()
+
+			data1 = dr.Item(i_kolumna_x)
+			matrix(0, i) = data1.ToString
+
+			matrix(i_kolumna_y, 0) = dr.Item(i_kolumna_y).ToString
+
+			i = i + 1
+
+			'o_chart.Series(i_seria).Points.AddXY(i, dr.Item(i_kolumna_x))
+			'o_chart.Series(i_seria).Points.AddXY(data1, dr.Item(i_kolumna_y))
+			o_chart.Series(i_seria).Points.AddXY(dr.Item(i_kolumna_x), dr.Item(i_kolumna_y))
+
+			ReDim Preserve matrix(aaa, i)
+
+		End While
+
+		'DataGridView1.DataSource = matrix
+
+		cmd.Dispose()
+		conn.Dispose()
+
+	End Sub
+
+
+	Protected Overrides Sub Finalize()
+		MyBase.Finalize()
+		dr.Dispose()
+
+	End Sub
+
+
+
+	Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+		If CheckBox1.Checked = False Then
+			SplitContainer2.Panel2.Visible = False
+			SplitContainer2.SplitterDistance = SplitContainer2.Height
+			CheckBox1.Text = "Wyświetl dane"
+		Else
+			SplitContainer2.Panel2.Visible = True
+			SplitContainer2.SplitterDistance = SplitContainer2.Height / 2
+			CheckBox1.Text = "Ukryj dane"
+		End If
+	End Sub
+
+
+	Private Sub Chart1_MouseDown(sender As System.Object, e As System.Windows.Forms.MouseEventArgs) Handles Chart1.MouseDown
+		p0 = e.Location
+		x0 = Date.FromOADate(Chart1.ChartAreas(0).AxisX.PixelPositionToValue(e.X))
+		mouseIsDown = True
+	End Sub
+	Private Sub Chart1_MouseUp(sender As System.Object, e As System.Windows.Forms.MouseEventArgs) Handles Chart1.MouseUp
+		Dim x As DateTime = Date.FromOADate(Chart1.ChartAreas(0).AxisX.PixelPositionToValue(e.X))
+		'make sure x0 < x
+		If x0 > x Then
+			Dim xtemp As DateTime = x
+			x = x0
+			x0 = xtemp
+		End If
+
+		'do your maths by iterating the points collection
+
+		'rescale the chart...
+		With Me.Chart1.ChartAreas(0).AxisX
+			.Minimum = x0.ToOADate()
+			.Maximum = x.ToOADate()
+		End With
+
+		mouseIsDown = False
+		Chart1.Invalidate()
+
+	End Sub
+
+
+	Private Sub Chart1_MouseMove(sender As Object, e As MouseEventArgs) Handles Chart1.MouseMove
+		If mouseIsDown Then
+			p = e.Location
+			Chart1.Invalidate()
+		End If
+	End Sub
+
+
+
+
+	Private Sub Chart1_Paint(sender As Object, e As PaintEventArgs) Handles Chart1.Paint
+		If mouseIsDown Then
+			Dim sze As New Size(p.X - p0.X, p.Y - p0.Y)
+			e.Graphics.DrawRectangle(Pens.Black, New Rectangle(p0, sze))
+		End If
+	End Sub
+
+
+
+
+
+
+
+
+
+
+
+End Class
+
